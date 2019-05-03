2affd79b7ff5218250bc572881a0a7f488dc57cd
Michal Jamry <jamrymi@gmail.com>
2019-05-03 15:27:31 +0200

    logging

--- find_duplicates_md5.py
+++ find_duplicates_md5.py
@@ -7,12 +7,14 @@ from concurrent.futures import ThreadPoolExecutor as PoolExecutor
 def log(s):
     print(s)
 
+
 def md5(fname):
     hash_md5 = hashlib.md5()
     with open(fname, "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
             hash_md5.update(chunk)
     return (fname, hash_md5.digest())
+
 
 def list_files_in_directory(dirr):
     log(f"Working directory: {dirr}")
@@ -22,12 +24,15 @@ def list_files_in_directory(dirr):
         if os.path.isfile(os.path.join(dirr, x))
     ]
 
+
 def calculate_sums(files):
     sums = list()
     with PoolExecutor(max_workers=os.cpu_count()) as executor:
         for ret in executor.map(md5, files):
+            log(f"Processed {len(sums)} out of  {len(files)}")
             sums.append(ret)
     return sums
+
 
 def find_duplicates(sums):
     seen = set()
@@ -39,19 +44,24 @@ def find_duplicates(sums):
             duplicates.append(file)
     return duplicates
 
+
 def write_out(duplicates, out_file):
     with open(out_file, "w") as f:
         for file in duplicates:
             f.write(file + "\n")
 
+
 def find_duplicate_files(dirr):
     all_files = list_files_in_directory(dirr)
+    log(f"No of files found: {len(all_files)}")
     sums = calculate_sums(all_files)
     return find_duplicates(sums)
+
 
 def find_and_write_out_duplicate_files(dirr, out):
     duplicates = find_duplicate_files(dirr)
     write_out(duplicates, out)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
